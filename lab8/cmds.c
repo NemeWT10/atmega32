732d5b6330d98393c83a134c4263500361717f6a
NemeWT10 <wojcikszymek@gmail.com>
2026-04-26 20:31:34 +0200

    Update cmds.c

--- lab8/cmds.c
+++ lab8/cmds.c
@@ -15,3 +15,12 @@ volatile uint8_t* LCD_PORT = &PORTB;
 #define CMD_DISPLAY_CLR 0x01
 #define CMD_SHIT_LEFT 0b00011000
 #define CMD_SHIT_RIGHT 0b00011100
+
+
+
+
+
+
+#define CMD_FUNCTION_SET 0b00101000 -> 8/4bit dataLength ; 2 linie ; font 5x8 ; - ; -
+#define CMD_ENTRY_MODE_SET 0b00000110 -> inkrementacja kursora ; brak przesuwania okna
+#define CMD_DISPLAY_ON 0b00001100 -> on/off ; kursor off ; brak migania
