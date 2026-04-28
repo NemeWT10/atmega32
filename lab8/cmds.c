aa801f9cff467d4c2bf002a24674547270463757
NemeWT10 <wojcikszymek@gmail.com>
2026-04-28 10:20:25 +0200

    Update cmds.c

--- lab8/cmds.c
+++ lab8/cmds.c
@@ -1,23 +1,24 @@
-volatile uint8_t* LCD_DDR = &DDRB;
-volatile uint8_t* LCD_PORT = &PORTB;
 #define RS_LCD 1
 #define EN_LCD 0
 #define D4_LCD 4
 #define D5_LCD 5
 #define D6_LCD 6
 #define D7_LCD 7
-#define CMD_INIT1 0b00110011
-#define CMD_INIT2 0b00110010
-#define CMD_FUNCTION_SET 0b00101000
-#define CMD_ENTRY_MODE_SET 0b00000110
-#define CMD_DISPLAY_ON 0b00001100
-#define CMD_RET_HOME 0x02
-#define CMD_DISPLAY_CLR 0x01
-#define CMD_SHIT_LEFT 0b00011000
-#define CMD_SHIT_RIGHT 0b00011100
+#define LCD_INIT1 0b00110011
+#define LCD_INIT2 0b00110010
+#define LCD_FUNCTION_SET 0b101000
+#define LCD_ENTRY_MODE_SET 0b110
+#define LCD_CLR 0x01
+#define LCD_RET_HOME 0b10
+#define LCD_DISPLAY_ON 0b1100
+#define LCD_DISPLAY_OFF 0b1000
+#define LCD_SHIFT_RIGHT 0b11100
+#define LCD_SHIFT_LEFT 0b11000
+#define LCD_SET_DDRAM_ADDR 0x80
+#define LCD_SET_CGRAM_ADDR 0x40
 
-
-
+volatile uint8_t *LCD_DDR = &DDRB;
+volatile uint8_t *LCD_PORT = &PORTB;
 
 
 
