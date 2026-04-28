e6a99de8eddacc76c8fd69c45e20ea46a7d323b2
NemeWT10 <wojcikszymek@gmail.com>
2026-04-28 10:18:50 +0200

    Update keypad.c

--- lab8/keypad.c
+++ lab8/keypad.c
@@ -5,16 +5,16 @@
 void keypad_init() {
 	//0b k4 k3 k2 k1 w4 w3 w2 w1
 	DDRA  =  0xF0; // wiersze wejscia, kolumny wyjscia
-	PORTA = 0xFF;            // pull-upy na wiersze, kolumny wysoko
+	PORTA = 0xFF;  // pull-upy na wiersze, kolumny wysoko
 }
 
 uint8_t keypad_scan() {
 	for(uint8_t col = 0;col < 4;col++)
 	{
 		PORTA |= 0xF0;
-		_delay_us(3);
+		_delay_us(5);
 		PORTA &=  ~(1 << (col + 4)); //stan niski na jednej kolumnie
-		_delay_us(3);
+		_delay_us(5);
 		for(uint8_t row = 0; row < 4 ; row++)
 		{
 			uint8_t odczyt = PINA & 0x0F;
